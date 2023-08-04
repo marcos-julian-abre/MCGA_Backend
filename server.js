f8c5c36141c773585b54dfe22a6e7366ff107833
Marcos Abre <marcosabre15@gmail.com>
2023-08-04 17:14:20 -0300

    'cors'

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const cors = require('cors');
 
 connectDB()
 
 const app = express()
 
+const corsOptions = {
+    origin: 'https://abremernmcga.onrender.com'
+  };
+  
+  app.use(cors(corsOptions));
+  
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
